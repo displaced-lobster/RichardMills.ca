1c948c2f15c73f567d02b95f4d3e187676cf1356
Richard Mills <scripts.richard@gmail.com>
2019-10-12 18:49:46 -0600

    Minify HTML.

--- .eleventy.js
+++ .eleventy.js
@@ -1,18 +1,31 @@
 const CleanCSS = require('clean-css');
+const HTMLMin = require('html-minifier');
 
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('images');
 
-    eleventyConfig.addFilter('cssmin', function(code) {
+    // Inline and minify CSS
+    eleventyConfig.addFilter('cssmin', code => {
         return new CleanCSS({}).minify(code).styles;
+    });
+
+    // Minify HTML
+    eleventyConfig.addTransform('htmlmin', (content, outputPath) => {
+        if (outputPath.endsWith('.html')) {
+            return HTMLMin.minify(content, {
+                useShortDocType: true,
+                removeComments: true,
+                collapseWhitespace: true
+            });
+        }
+        return content;
     });
 
     return {
         templateFormats: [
             'md',
             'njk',
-            'html',
-            'css'
+            'html'
         ],
         markdownTemplateEngine: 'njk',
         htmlTemaplteEngine: 'njk',
